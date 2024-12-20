d100ff270e549d577ef314e20593e06128c16d0f
Shyju <shyjuchathampalli@gmail.com>
2024-12-20 10:18:13 +0530

    changed top bar in mobile view

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,7 +4,7 @@ import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa'; // Import icons
 const ContactInfo = () => {
   return (
     <div className="fixed top-0 z-20 w-full flex items-center justify-between p-2 px-4 bg-gray-100">
-      <div className="flex items-center text-gray-600"> {/* Set text color to #999 */}
+      <div className="flex items-center text-gray-600 hidden md:flex"> {/* Set text color to #999 */}
         <FaPhoneAlt className="mr-2" />
         <span className="text-sm">+91 97784 58883</span>
       </div>
