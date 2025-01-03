ab0c89f687ec339f08a9135792f4f85666fac207
Shyju <shyjuchathampalli@gmail.com>
2025-01-03 21:01:47 +0530

    helmet added and included in the home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'; // Import Router
 import App from './App.jsx';
 import './index.css';
+import { Helmet } from 'react-helmet';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <Helmet>
+        <title>Hasta Wellness | Best Dental clinic and Skin care centre Trivandrum</title>
+        <meta name='description' content='One of the best dental-skin clinic Offering cosmetic dentistry, smile designing, laser hair removal, prp therapy, carbon peels and chemical peels in Kazhakoottam, Trivandrum.'/>
+        <meta name='kewords' content='prp therapy, skin care trivandrum, dental clinic trivandrum, laser hair removal, chemical peels, carbon peels, smile designing, mesotherapy' />
+      </Helmet>
       <App />
     </Router>
   </React.StrictMode>,
